db7be582988629f943b5959035dec129c9d400b3
jcharnley <jasoncharnley@gmail.com>
2018-11-30 10:50:35 +0000

    Fetch data from the server to the front!

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
-alert("hi I'm main js");
+var skills = document.querySelector("#skills");
+var button = document.querySelector(".button");
+var heading = document.querySelector("#heading");
 
-// fetchData(function(result) {});
+fetchData(function(result) {
+  // console.log("I got in the main.", result);
+  button.addEventListener("click", function() {
+    var h2 = document.createElement("h2");
+    console.log(result);
+    h2.textContent = result[0].slug;
+    heading.appendChild(h2);
+  });
+});
