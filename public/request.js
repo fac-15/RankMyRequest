1168a8f248e875b66b0972dbff8f7aa77cb311e4
susanX <susan.gooding@yahoo.co.uk>
2018-11-30 12:46:24 +0000

    api2 added

--- public/request.js
+++ public/request.js
@@ -1,12 +1,26 @@
 function fetchData(cb) {
   var xhr = new XMLHttpRequest();
-  console.log(xhr);
+  // console.log(xhr);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log(xhr.responseText);
+      // console.log(xhr.responseText);
       cb(JSON.parse(xhr.responseText));
     }
   };
   xhr.open("GET", "/onload", true);
   xhr.send();
 }
+
+function fetchData2() {
+  var xhr = new XMLHttpRequest();
+  console.log(xhr);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+      JSON.parse(xhr.responseText);
+    }
+  };
+  xhr.open("GET", "/onload2", true);
+  xhr.send();
+}
+fetchData2();
--- src/router.js
+++ src/router.js
@@ -9,6 +9,9 @@ const router = (request, response) => {
   } else if (url === "/onload") {
     console.log("I have been reached");
     handler.apiHandler(request, response, url);
+  } else if (url === "/onload2") {
+    console.log("onload2 has been reached");
+    handler.apiHandler2(request, response);
   } else {
     response.writeHead(404, "Content-Type: text/html");
     response.end("<h1>404 file not found</h1>");
